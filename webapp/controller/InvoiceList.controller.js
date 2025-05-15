98c090dd6c5b84fafbbc5acb74f1a3a09e829cdd
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2025-05-15 17:47:56 +0000

    create objectStatus

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter"
+], function (Controller, JSONModel, Formatter) {
     "use strict";
     return Controller.extend("study.controller.InvoiceList", {
+        formatter: Formatter,
         onInit : function () {
             var oViewModel = new JSONModel({
                 currency: "EUR"
