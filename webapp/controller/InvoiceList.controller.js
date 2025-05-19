bc013e2123ec85c31503298189964bb737b26f50
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2025-05-19 16:08:30 +0000

    feature: create filter

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,11 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
-], function (Controller, JSONModel, Formatter) {
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Formatter, Filter, FilterOperator) {
     "use strict";
     return Controller.extend("study.controller.InvoiceList", {
         formatter: Formatter,
+        onFilterInvoices: function (oEvent) {
+            // build filter array 
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if(sQuery){
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+
+            //filter binding
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        },
         onInit : function () {
             var oViewModel = new JSONModel({
                 currency: "EUR"
